34edbef7c3f95ab8619c120086de5e02d903ea0d
Tarek Adam <tarek.adam@gmail.com>
2022-03-07 14:04:57 -0600

    start

---
+++ README.md
@@ -0,0 +1,1 @@
+# facebook-conversions-api
---
+++ src/EventServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace BoneCreative\FacebookConversionsApi;
+
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as BaseServiceProvider;
+
+class EventServiceProvider extends BaseServiceProvider
+{
+	protected $listen = [
+	];
+	
+	protected $subscribe = [
+	];
+	
+	public function boot()
+	{
+		parent::boot();
+	}
+}
