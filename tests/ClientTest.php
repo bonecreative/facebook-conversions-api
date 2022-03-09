3800fb7e44d59d78f7e1e50a235fbabc2cd26fee
Tarek Adam <tarek.adam@gmail.com>
2022-03-08 22:15:52 -0600

    adds facebook test suite

---
+++ tests/ClientTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Bone\Test;
+
+use Tests\TestCase;
+
+class ClientTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @test
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
