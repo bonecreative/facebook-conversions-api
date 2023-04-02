1282d58b65ef3ae97a0fc7df7f25122d5ac207dc
Tarek Adam <tarek.adam@gmail.com>
2023-04-02 13:32:37 -0600

    nests route

--- routes/main.php
+++ routes/main.php
@@ -2,7 +2,7 @@
 Route::group(['as' => 'analytics::', 'prefix' => 'public-api', 'namespace' => '\\BoneCreative\\FacebookConversionsApi', 'middleware' => ['cors', 'api']], function ()
 {
 
-	Route::match(['options', 'post'], 'analytics', [
+	Route::match(['options', 'post'], 'analytics/fb', [
 		'as'   => 'fb',
 		'uses' => 'Controller'
 	]);
